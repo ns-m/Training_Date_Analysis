b0f9fe22d757c1181444b0cfb8b48bdeff1fa0e5
kmv <9629095@gmail.com>
2020-06-11 16:19:02 +0300

    Commit 2

--- Skillfactory/skillfactory_c1_3_4.py
+++ Skillfactory/skillfactory_c1_3_4.py
@@ -88,111 +88,154 @@
 # print(writing_count_lunch/writing_count*100)
 
 
-students = {}
+# students = {}
+#
+# f = open('StudentsPerformance.csv')
+#
+# for line in f:
+#     info = line.split(',')
+#     if info[0] == '"gender"':
+#         continue
+#     else:
+#         ethnicity = info[1][1:-1]
+#         if ethnicity in students:
+#             students[ethnicity] += 1
+#         else:
+#             students[ethnicity] = 1
+# print(students['group A'])
+#
+# f2 = open('StudentsPerformance.csv')
+# rating_a_math = []
+# rating_a_reading = []
+# rating_a_writing = []
+# rating_b_math = []
+# rating_b_reading = []
+# rating_b_writing = []
+# rating_c_math = []
+# rating_c_reading = []
+# rating_c_writing = []
+# rating_d_math = []
+# rating_d_reading = []
+# rating_d_writing = []
+# rating_e_math = []
+# rating_e_reading = []
+# rating_e_writing = []
+# for line2 in f2:
+#     info2= line2.split(',')
+#     if info2[0] == '"gender"':
+#         continue
+#     if info2[1] == '"group A"':
+#         mark1 = int(info2[5][1:-1])
+#         mark2 = int(info2[6][1:-1])
+#         mark3 = int(info2[7][1:-2])
+#         rating_a_math.append(mark1)
+#         rating_a_reading.append(mark2)
+#         rating_a_writing.append(mark3)
+#     if info2[1] == '"group B"':
+#         mark1 = int(info2[5][1:-1])
+#         mark2 = int(info2[6][1:-1])
+#         mark3 = int(info2[7][1:-2])
+#         rating_b_math.append(mark1)
+#         rating_b_reading.append(mark2)
+#         rating_b_writing.append(mark3)
+#     if info2[1] == '"group C"':
+#         mark1 = int(info2[5][1:-1])
+#         mark2 = int(info2[6][1:-1])
+#         mark3 = int(info2[7][1:-2])
+#         rating_c_math.append(mark1)
+#         rating_c_reading.append(mark2)
+#         rating_c_writing.append(mark3)
+#     if info2[1] == '"group D"':
+#         mark1 = int(info2[5][1:-1])
+#         mark2 = int(info2[6][1:-1])
+#         mark3 = int(info2[7][1:-2])
+#         rating_d_math.append(mark1)
+#         rating_d_reading.append(mark2)
+#         rating_d_writing.append(mark3)
+#     if info2[1] == '"group E"':
+#         mark1 = int(info2[5][1:-1])
+#         mark2 = int(info2[6][1:-1])
+#         mark3 = int(info2[7][1:-2])
+#         rating_e_math.append(mark1)
+#         rating_e_reading.append(mark2)
+#         rating_e_writing.append(mark3)
+#
+# math_avg_a = sum(rating_a_math)/len(rating_a_math)
+# reading_avg_a = sum(rating_a_reading)/len(rating_a_reading)
+# writing_avg_a = sum(rating_a_writing)/len(rating_a_writing)
+# total_a = (math_avg_a + reading_avg_a + writing_avg_a)/3
+#
+# math_avg_b = sum(rating_b_math)/len(rating_b_math)
+# reading_avg_b = sum(rating_b_reading)/len(rating_b_reading)
+# writing_avg_b = sum(rating_b_writing)/len(rating_b_writing)
+# total_b = (math_avg_b + reading_avg_b + writing_avg_b)/3
+#
+# math_avg_c = sum(rating_c_math)/len(rating_c_math)
+# reading_avg_c = sum(rating_c_reading)/len(rating_c_reading)
+# writing_avg_c = sum(rating_c_writing)/len(rating_c_writing)
+# total_c = (math_avg_c + reading_avg_c + writing_avg_c)/3
+#
+# math_avg_d = sum(rating_d_math)/len(rating_d_math)
+# reading_avg_d = sum(rating_d_reading)/len(rating_d_reading)
+# writing_avg_d = sum(rating_d_writing)/len(rating_d_writing)
+# total_d = (math_avg_d + reading_avg_d + writing_avg_d)/3
+#
+# math_avg_e = sum(rating_e_math)/len(rating_e_math)
+# reading_avg_e = sum(rating_e_reading)/len(rating_e_reading)
+# writing_avg_e = sum(rating_e_writing)/len(rating_e_writing)
+# total_e = (math_avg_e + reading_avg_e + writing_avg_e)/3
+#
+# for group, number in students.items():
+#     if group == 'group A':
+#         print(f'{group}: Count - {number}, Math - {math_avg_a:.2f}, Reading - {reading_avg_a:.2f}, Writing - {writing_avg_a:.2f}, Total - {total_a:.2f}')
+#     if group == 'group B':
+#         print(f'{group}: Count - {number}, Math - {math_avg_b:.2f}, Reading - {reading_avg_b:.2f}, Writing - {writing_avg_b:.2f}, Total - {total_b:.2f}')
+#     if group == 'group C':
+#         print(f'{group}: Count - {number}, Math - {math_avg_c:.2f}, Reading - {reading_avg_c:.2f}, Writing - {writing_avg_c:.2f}, Total - {total_c:.2f}')
+#     if group == 'group D':
+#         print(f'{group}: Count - {number}, Math - {math_avg_d:.2f}, Reading - {reading_avg_d:.2f}, Writing - {writing_avg_d:.2f}, Total - {total_d:.2f}')
+#     if group == 'group E':
+#         print(f'{group}: Count - {number}, Math - {math_avg_e:.2f}, Reading - {reading_avg_e:.2f}, Writing - {writing_avg_e:.2f}, Total - {total_e:.2f}')
 
-f = open('StudentsPerformance.csv')
 
-for line in f:
-    info = line.split(',')
-    if info[0] == '"gender"':
-        continue
-    else:
-        ethnicity = info[1][1:-1]
-        if ethnicity in students:
-            students[ethnicity] += 1
-        else:
-            students[ethnicity] = 1
-print(students['group A'])
+# def get_median(numbers):
+#     nums = sorted(numbers)
+#     if len(nums)%2 != 0:
+#         result = nums[int(len(numbers)/2)]
+#     else:
+#         result = (nums[int(len(nums)/2-1)] + nums[int(len(nums)/2)])/2
+#     return result
+#
+# print(get_median([3, 3, 4, 4]))
 
-f2 = open('StudentsPerformance.csv')
-rating_a_math = []
-rating_a_reading = []
-rating_a_writing = []
-rating_b_math = []
-rating_b_reading = []
-rating_b_writing = []
-rating_c_math = []
-rating_c_reading = []
-rating_c_writing = []
-rating_d_math = []
-rating_d_reading = []
-rating_d_writing = []
-rating_e_math = []
-rating_e_reading = []
-rating_e_writing = []
-for line2 in f2:
-    info2= line2.split(',')
-    if info2[0] == '"gender"':
-        continue
-    if info2[1] == '"group A"':
-        mark1 = int(info2[5][1:-1])
-        mark2 = int(info2[6][1:-1])
-        mark3 = int(info2[7][1:-2])
-        rating_a_math.append(mark1)
-        rating_a_reading.append(mark2)
-        rating_a_writing.append(mark3)
-    if info2[1] == '"group B"':
-        mark1 = int(info2[5][1:-1])
-        mark2 = int(info2[6][1:-1])
-        mark3 = int(info2[7][1:-2])
-        rating_b_math.append(mark1)
-        rating_b_reading.append(mark2)
-        rating_b_writing.append(mark3)
-    if info2[1] == '"group C"':
-        mark1 = int(info2[5][1:-1])
-        mark2 = int(info2[6][1:-1])
-        mark3 = int(info2[7][1:-2])
-        rating_c_math.append(mark1)
-        rating_c_reading.append(mark2)
-        rating_c_writing.append(mark3)
-    if info2[1] == '"group D"':
-        mark1 = int(info2[5][1:-1])
-        mark2 = int(info2[6][1:-1])
-        mark3 = int(info2[7][1:-2])
-        rating_d_math.append(mark1)
-        rating_d_reading.append(mark2)
-        rating_d_writing.append(mark3)
-    if info2[1] == '"group E"':
-        mark1 = int(info2[5][1:-1])
-        mark2 = int(info2[6][1:-1])
-        mark3 = int(info2[7][1:-2])
-        rating_e_math.append(mark1)
-        rating_e_reading.append(mark2)
-        rating_e_writing.append(mark3)
+# user_db = [{'orders': 12}, {'orders': 30}, {'orders': 45}]
+#
+# # перепишите эту часть
+# order_sum = sum([user['orders'] for user in user_db])
+# orders_per_user = order_sum/len(user_db)
+# print(orders_per_user)
 
-math_avg_a = sum(rating_a_math)/len(rating_a_math)
-reading_avg_a = sum(rating_a_reading)/len(rating_a_reading)
-writing_avg_a = sum(rating_a_writing)/len(rating_a_writing)
-total_a = (math_avg_a + reading_avg_a + writing_avg_a)/3
+# values = [1, 2, 3]
+# vectors = [(10, 3), (4, 5), (6, 7)]
+# a1 = list(map(lambda x: x+2, [1, 2, 3]))
+# # a2 = map(values, lambda x: x**0.5)
+# a3 = list(map(lambda vec: (vec[0]**2 + vec[1]**2)**0.5, vectors))
+# # a4 = list(map(lambda x, y: x + y, values))
+#
+# print(a3)
 
-math_avg_b = sum(rating_b_math)/len(rating_b_math)
-reading_avg_b = sum(rating_b_reading)/len(rating_b_reading)
-writing_avg_b = sum(rating_b_writing)/len(rating_b_writing)
-total_b = (math_avg_b + reading_avg_b + writing_avg_b)/3
+# values = [4, 8, 15, 16, 23, 42]
+# mean = 18
+#
+# result = map(lambda x: x - mean, values)
+# print(list(result))
 
-math_avg_c = sum(rating_c_math)/len(rating_c_math)
-reading_avg_c = sum(rating_c_reading)/len(rating_c_reading)
-writing_avg_c = sum(rating_c_writing)/len(rating_c_writing)
-total_c = (math_avg_c + reading_avg_c + writing_avg_c)/3
+# def normalize(numbers, mean=0, std=1):
+#     result = [int((x - mean)/std) for x in numbers]
+#     return result
+# print(normalize([10, 20], mean=15))
 
-math_avg_d = sum(rating_d_math)/len(rating_d_math)
-reading_avg_d = sum(rating_d_reading)/len(rating_d_reading)
-writing_avg_d = sum(rating_d_writing)/len(rating_d_writing)
-total_d = (math_avg_d + reading_avg_d + writing_avg_d)/3
-
-math_avg_e = sum(rating_e_math)/len(rating_e_math)
-reading_avg_e = sum(rating_e_reading)/len(rating_e_reading)
-writing_avg_e = sum(rating_e_writing)/len(rating_e_writing)
-total_e = (math_avg_e + reading_avg_e + writing_avg_e)/3
-
-for group, number in students.items():
-    if group == 'group A':
-        print(f'{group}: Count - {number}, Math - {math_avg_a:.2f}, Reading - {reading_avg_a:.2f}, Writing - {writing_avg_a:.2f}, Total - {total_a:.2f}')
-    if group == 'group B':
-        print(f'{group}: Count - {number}, Math - {math_avg_b:.2f}, Reading - {reading_avg_b:.2f}, Writing - {writing_avg_b:.2f}, Total - {total_b:.2f}')
-    if group == 'group C':
-        print(f'{group}: Count - {number}, Math - {math_avg_c:.2f}, Reading - {reading_avg_c:.2f}, Writing - {writing_avg_c:.2f}, Total - {total_c:.2f}')
-    if group == 'group D':
-        print(f'{group}: Count - {number}, Math - {math_avg_d:.2f}, Reading - {reading_avg_d:.2f}, Writing - {writing_avg_d:.2f}, Total - {total_d:.2f}')
-    if group == 'group E':
-        print(f'{group}: Count - {number}, Math - {math_avg_e:.2f}, Reading - {reading_avg_e:.2f}, Writing - {writing_avg_e:.2f}, Total - {total_e:.2f}')
+# def sum_args(*args):
+#     result = sum(args)
+#     return result
+# print(sum_args(10, 15, -4))
